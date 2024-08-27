aafa01cf3d504b160b53656a1beef6eaa78a108a
opilane <opilane@HP-CZC2349HT9>
2024-08-27 09:15:14 +0300

    ghfh

--- MauiApp_TARgv23/TextPage.xaml.cs
+++ MauiApp_TARgv23/TextPage.xaml.cs
@@ -1,9 +1,32 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace MauiApp_TARgv23;
 
 public partial class TextPage : ContentPage
 {
-	public TextPage()
+    Label lbl;
+    Editor editor;
+    Button btn_tagasi, btn_edasi, btn_algus;
+    public TextPage()
 	{
-		InitializeComponent();
+        lbl = new Label()
+        {
+            Text = "Pealkiri",
+            TextColor = Color.FromRgb(200, 0, 0),
+            FontFamily = "Socafe 400",
+        };
+        editor = new Editor
+        {
+            Placeholder = "Vihje:Sisesta siia tekst",
+            PlaceholderColor = Color.FromRgb(50, 100, 100),
+            TextColor = Color.FromRgb(200, 0, 0),
+            BackgroundColor = Color.FromRgb(10, 100, 200)
+        };
+        VerticalStackLayout vst = new VerticalStackLayout
+        {
+            Children = { lbl, editor, btn }
+        };
+
+		
 	}
 }
